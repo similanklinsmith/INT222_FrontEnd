f8985598320ce3aa7e7fcfb4b8a2447d1ff87d5c
Similan Klinsmith <similan.klinsmith@mail.kmutt.ac.th>
2021-09-20 14:02:26 +0700

    add Table Component

---
+++ src/components/Table.vue
@@ -0,0 +1,117 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="th in ths" :key="th">{{ th }}</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <slot></slot>
+  </table>
+</template>
+<script>
+export default {
+  name: "Table",
+  props: ["ths"],
+};
+</script>
+<style scoped>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+thead {
+  font-size: 1.4rem;
+  text-transform: uppercase;
+  background-color: #333;
+  color: white;
+}
+th {
+  font-weight: 400;
+  padding: 2rem;
+}
+th:nth-child(even) {
+  background-color: #555;
+}
+/* below 872px */
+@media (max-width: 55em) {
+  .color-inputs {
+    row-gap: 1.2rem;
+  }
+  .options {
+    justify-items: start;
+  }
+}
+/* below 542px */
+@media (max-width: 34em) {
+  .switch-btn {
+    font-size: 1.2rem;
+  }
+  .options {
+    justify-items: center;
+  }
+  .color-inputs .example-color {
+    display: none;
+  }
+  .table {
+    margin: 4.8rem 0;
+    width: 100%;
+  }
+  th {
+    padding: 1.2rem;
+  }
+  thead,
+  tbody {
+    font-size: 1.2rem;
+  }
+}
+/* below 450px */
+@media (max-width: 28em) {
+  input {
+    width: 12rem;
+    font-size: 1.4rem;
+  }
+  .color-inputs {
+    gap: 1.6rem;
+  }
+  .tertiary-header {
+    display: none;
+  }
+  .options {
+    width: 100%;
+    margin: 0%;
+  }
+  .switch-btn {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 0;
+    width: 100%;
+  }
+  .switch-btn .btn-brands,
+  .switch-btn .btn-colors {
+    width: 100%;
+  }
+
+  .switch-btn .btn-colors {
+    border-top-left-radius: 2.4rem;
+    border-bottom-left-radius: 2.4rem;
+  }
+  .switch-btn .btn-brands {
+    border-top-right-radius: 2.4rem;
+    border-bottom-right-radius: 2.4rem;
+  }
+  .example-color {
+    width: 1.6rem;
+    height: 1.6rem;
+  }
+  .table {
+    margin: 2.4rem 0;
+  }
+  th {
+    padding: 1rem;
+  }
+  tbody td {
+    padding: 1.8rem;
+  }
+}
+</style>
